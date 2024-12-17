bfed8242ca5edafc0bf8ca4dcab661fe1dd0f6df
Digital Sparks B.V. <122462234+digital-sparks@users.noreply.github.com>
2024-12-17 11:55:14 +0100

    update preloader

--- src/global.js
+++ src/global.js
@@ -1,29 +1,18 @@
 import Swiper from 'swiper';
-import { Parallax, Keyboard, Mousewheel, Autoplay } from 'swiper/modules';
+import { Navigation, Keyboard, Mousewheel, Autoplay, EffectFade } from 'swiper/modules';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { SplitText } from 'gsap/SplitText';
-gsap.registerPlugin(SplitText, ScrollTrigger);
+import CustomEase from 'gsap/CustomEase';
+gsap.registerPlugin(ScrollTrigger, CustomEase);
 import Lenis from 'lenis';
 
 window.Webflow ||= [];
 Webflow.push(() => {
-  console.log('hello (global.js)');
-
   // ————— LENIS ————— //
   ('use strict'); // fix lenis in safari
   let lenis;
   if (Webflow.env('editor') === undefined) {
     lenis = new Lenis();
-
-    // // useful for when you want to stop the lenis scroll, e.g. when opening a modal
-    // $('[data-lenis-stop]').on('click', function () {
-    //   lenis.stop();
-    // });
-
-    // $('[data-lenis-start]').on('click', function () {
-    //   lenis.start();
-    // });
 
     lenis.on('scroll', ScrollTrigger.refresh);
 
@@ -54,7 +43,6 @@ Webflow.push(() => {
           end: 'bottom top',
           scrub: true,
           // markers: true,
-          // toggleActions: 'play none resume reverse',
         },
       }
     );
@@ -126,8 +114,12 @@ Webflow.push(() => {
   // ————— LINK HOVER ANIMATION ————— //
 
   // ————— PRELOADER ANIMATION ————— //
-  let preloaderAnimation = gsap.timeline();
-  document.querySelectorAll('.preloader_component').forEach((preloader) => {
+
+  const preloader = document.querySelector('.preloader_component') || '';
+
+  if (preloader) {
+    let preloaderAnimation = gsap.timeline();
+    CustomEase.create('preloaderEase', '.5, 0, .25, 1');
     const cover = preloader.querySelectorAll('.preloader_cover');
     const content = preloader.querySelector('.preloader_content');
 
@@ -140,16 +132,16 @@ Webflow.push(() => {
         },
         {
           xPercent: -3,
-          duration: 1.25,
-          delay: 0.5,
-          ease: 'power4.out',
+          duration: 1.1,
+          delay: 0.2,
+          ease: 'power3.inOut',
         }
       )
       .to(content, {
         opacity: 0,
-        delay: 0,
-        duration: 0.5,
-        ease: 'power2.out',
+        delay: 0.15,
+        duration: 0.4,
+        ease: 'none',
       })
       .set(preloader, {
         display: 'none',
@@ -157,32 +149,30 @@ Webflow.push(() => {
       .set(cover, {
         opacity: 0,
       });
-  });
 
-  document.querySelectorAll('a').forEach(function (anchor) {
-    anchor.addEventListener('click', function (e) {
-      if (
-        this.hostname === window.location.host &&
-        this.getAttribute('href').indexOf('#') === -1 &&
-        this.getAttribute('target') !== '_blank'
-      ) {
-        e.preventDefault();
-        const destination = this.getAttribute('href');
-        const preloader = document.querySelector('.preloader_component');
-        const content = preloader.querySelector('.preloader_content');
+    document.querySelectorAll('a').forEach(function (anchor) {
+      anchor.addEventListener('click', function (e) {
+        if (
+          this.hostname === window.location.host &&
+          this.getAttribute('href').indexOf('#') === -1 &&
+          this.getAttribute('target') !== '_blank'
+        ) {
+          e.preventDefault();
+          const destination = this.getAttribute('href');
 
-        gsap.set(preloader, { display: 'block' });
-        gsap.to(content, {
-          opacity: 1,
-          duration: 0.5,
-          ease: 'power2.out',
-          onComplete: () => {
-            window.location = destination;
-          },
-        });
-      }
+          gsap.set(preloader, { display: 'block' });
+          gsap.to(content, {
+            opacity: 1,
+            duration: 0.35,
+            ease: 'none',
+            onComplete: () => {
+              window.location = destination;
+            },
+          });
+        }
+      });
     });
-  });
+  }
 
   // On click of the back button
   window.onpageshow = function (event) {
@@ -191,4 +181,215 @@ Webflow.push(() => {
     }
   };
   // ————— PRELOADER ANIMATION ————— //
+
+  // ————— SERVICES IMAGE PARALLAX EFFECT ————— //
+  document.querySelectorAll('.image-absolute.is-services').forEach((image) => {
+    // parallax effect on y scroll
+    gsap.fromTo(
+      image,
+      {
+        yPercent: -6,
+      },
+      {
+        yPercent: 6,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: image.parentNode,
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: true,
+          // markers: true,
+        },
+      }
+    );
+  });
+  // ————— SERVICES IMAGE PARALLAX EFFECT ————— //
+
+  // ––––– SERVICE SWIPER ————— //
+  if (document.querySelectorAll('.services-swiper_wrapper')) {
+    const servicesSwiper = new Swiper('.services-swiper_wrapper', {
+      modules: [Navigation, Keyboard, Mousewheel, Autoplay],
+      wrapperClass: 'services-swiper_list',
+      slideClass: 'services-swiper_item',
+      slidesPerView: 'auto',
+      direction: 'horizontal',
+      spaceBetween: 32,
+      grabCursor: true,
+      loop: false,
+      speed: 550,
+      // parallax: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      // autoplay: {
+      //   enabled: false,
+      //   delay: 4000,
+      //   pauseOnMouseEnter: false,
+      //   disableOnInteraction: true,
+      // },
+      navigation: {
+        nextEl: '.swiper_button.is-next',
+        prevEl: '.swiper_button.is-prev',
+      },
+      breakpoints: {
+        768: {
+          speed: 900,
+          spaceBetween: 32,
+          mousewheel: {
+            enabled: true,
+            forceToAxis: true,
+            thresholdDelta: 5,
+          },
+        },
+      },
+      on: {
+        beforeInit: (swiper) => {
+          swiper.wrapperEl.style.gridColumnGap = 'unset';
+        },
+      },
+    });
+  }
+  // ––––– SERVICE SWIPER ————— //
+
+  // ––––– LOCATIONS SWIPER ————— //
+  if (document.querySelectorAll('.locations-swiper_wrapper')) {
+    const locationsSwiper = new Swiper('.locations-swiper_wrapper', {
+      modules: [Navigation, Keyboard, Mousewheel, Autoplay],
+      wrapperClass: 'locations-swiper_list',
+      slideClass: 'locations-swiper_item',
+      slidesPerView: 'auto',
+      direction: 'horizontal',
+      spaceBetween: 40,
+      grabCursor: true,
+      loop: false,
+      speed: 550,
+      // parallax: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      // autoplay: {
+      //   enabled: false,
+      //   delay: 4000,
+      //   pauseOnMouseEnter: false,
+      //   disableOnInteraction: true,
+      // },
+      navigation: {
+        nextEl: '.swiper_button.is-next',
+        prevEl: '.swiper_button.is-prev',
+      },
+      breakpoints: {
+        768: {
+          speed: 900,
+          spaceBetween: 40,
+          mousewheel: {
+            enabled: true,
+            forceToAxis: true,
+            thresholdDelta: 5,
+          },
+        },
+      },
+      on: {
+        beforeInit: (swiper) => {
+          swiper.wrapperEl.style.gridColumnGap = 'unset';
+        },
+      },
+    });
+  }
+  // ––––– LOCATIONS SWIPER ————— //
+
+  // ––––– CLIENTS SWIPER ————— //
+  if (document.querySelectorAll('.clients-swiper_wrapper')) {
+    const clientsSwiper = new Swiper('.clients-swiper_wrapper', {
+      modules: [Navigation, Keyboard, Mousewheel, Autoplay],
+      wrapperClass: 'clients-swiper_list',
+      slideClass: 'clients-swiper_item',
+      slidesPerView: 'auto',
+      direction: 'horizontal',
+      spaceBetween: 60,
+      grabCursor: true,
+      loop: true,
+      speed: 550,
+      // parallax: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      autoplay: {
+        enabled: true,
+        delay: 4000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: true,
+      },
+      navigation: {
+        nextEl: '.swiper_button.is-next',
+        prevEl: '.swiper_button.is-prev',
+      },
+      breakpoints: {
+        768: {
+          speed: 900,
+          spaceBetween: 60,
+          mousewheel: {
+            enabled: true,
+            forceToAxis: true,
+            thresholdDelta: 5,
+          },
+        },
+      },
+      on: {
+        beforeInit: (swiper) => {
+          swiper.wrapperEl.style.gridColumnGap = 'unset';
+        },
+      },
+    });
+  }
+  // ––––– CLIENTS SWIPER ————— //
+
+  // ––––– TESTIMONIALS SWIPER ————— //
+  if (document.querySelectorAll('.testimonial-swiper_wrapper')) {
+    const testimonialSwiper = new Swiper('.testimonial-swiper_wrapper', {
+      modules: [Navigation, Keyboard, Mousewheel, Autoplay, EffectFade],
+      wrapperClass: 'testimonial-swiper_list',
+      slideClass: 'testimonial-swiper_item',
+      slidesPerView: 'auto',
+      effect: 'fade',
+      direction: 'horizontal',
+      spaceBetween: 0,
+      grabCursor: true,
+      loop: true,
+      speed: 300,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      autoplay: {
+        enabled: true,
+        delay: 4000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: true,
+      },
+      navigation: {
+        nextEl: '.swiper_button.is-next',
+        prevEl: '.swiper_button.is-prev',
+      },
+      breakpoints: {
+        768: {
+          speed: 300,
+          spaceBetween: 0,
+          mousewheel: {
+            enabled: true,
+            forceToAxis: true,
+            thresholdDelta: 5,
+          },
+        },
+      },
+      on: {
+        beforeInit: (swiper) => {
+          swiper.wrapperEl.style.gridColumnGap = 'unset';
+        },
+      },
+    });
+  }
+  // ––––– TESTIMONIALS SWIPER ————— //
 });
